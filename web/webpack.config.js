bd269b66cb25bc8c87cc968808e536a5f27a6fa5
Sitnikov A.A <sitnikov.andrei@mail.ru>
2019-02-27 22:44:14 +0300

    мелкие изменения стилей

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -3,33 +3,50 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
-    output: {
+    output:
+    {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    devServer: {
+    devServer:
+    {
         contentBase: './dist'
     },
     mode: 'development',
-    module: {
-        rules: [{
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
+    module:
+    {
+        rules: [
+        {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract(
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader']
+            })
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract(
             {
-                test: /\.(png|jpg|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {},
-                }, ],
-            }
-        ]
+                fallback: "style-loader",
+                use: "css-loader"
+            })
+        },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+            {
+                loader: 'file-loader',
+                options:
+                {},
+            }, ],
+        }]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html' })
+        new HtmlWebpackPlugin(
+        {
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin('style.css')
     ]
 };
